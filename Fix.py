7325a0cf03a00ec19a2559c6e85e06c1caecb829
Aditya Anand <adianand1729@gmail.com>
2025-01-03 15:05:09 +0530

    Some bug fixes

--- Fix.py
+++ Fix.py
@@ -22,4 +22,5 @@ def repair_notebook(file_path):
 
 
 # Replace 'Cancer detect.ipynb' with your file name
-repair_notebook("Linear Regression.ipynb")
+repair_notebook("Multiple Linear Regression.ipynb")
+
